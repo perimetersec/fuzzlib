6cc25c8dfac6a1d2a1534b3dd16b924238cfdcb6
Rappie <jasper@rappie.com>
2023-12-18 14:57:41 +0100

    Split off FuzzLibString

--- src/PropertiesHelper.sol
+++ src/PropertiesHelper.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.0;
 
+import "./FuzzLibString.sol";
+
 abstract contract PropertiesAsserts {
     event LogUint256(string, uint256);
     event LogAddress(string, address);
@@ -27,8 +29,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (a != b) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -49,8 +51,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (a != b) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -71,8 +73,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (a == b) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -93,8 +95,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (a == b) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -115,8 +117,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a >= b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -137,8 +139,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a >= b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -159,8 +161,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a > b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -181,8 +183,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a > b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -203,8 +205,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a <= b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -225,8 +227,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a <= b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -247,8 +249,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a < b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -269,8 +271,8 @@ abstract contract PropertiesAsserts {
         string memory reason
     ) internal {
         if (!(a < b)) {
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory bStr = PropertiesLibString.toString(b);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory bStr = FuzzLibString.toString(b);
             bytes memory assertMsg = abi.encodePacked(
                 "Invalid: ",
                 aStr,
@@ -292,8 +294,8 @@ abstract contract PropertiesAsserts {
     ) internal returns (uint256) {
         if (value < low || value > high) {
             uint256 ans = low + (value % (high - low + 1));
-            string memory valueStr = PropertiesLibString.toString(value);
-            string memory ansStr = PropertiesLibString.toString(ans);
+            string memory valueStr = FuzzLibString.toString(value);
+            string memory ansStr = FuzzLibString.toString(ans);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 valueStr,
@@ -317,8 +319,8 @@ abstract contract PropertiesAsserts {
             int256 clamped = (value - low) % (range);
             if (clamped < 0) clamped += range;
             int256 ans = low + clamped;
-            string memory valueStr = PropertiesLibString.toString(value);
-            string memory ansStr = PropertiesLibString.toString(ans);
+            string memory valueStr = FuzzLibString.toString(value);
+            string memory ansStr = FuzzLibString.toString(ans);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 valueStr,
@@ -340,8 +342,8 @@ abstract contract PropertiesAsserts {
                 "clampLt cannot clamp value a to be less than zero. Check your inputs/assumptions."
             );
             uint256 value = a % b;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -358,8 +360,8 @@ abstract contract PropertiesAsserts {
     function clampLt(int256 a, int256 b) internal returns (int256) {
         if (!(a < b)) {
             int256 value = b - 1;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -376,8 +378,8 @@ abstract contract PropertiesAsserts {
     function clampLte(uint256 a, uint256 b) internal returns (uint256) {
         if (!(a <= b)) {
             uint256 value = a % (b + 1);
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -394,8 +396,8 @@ abstract contract PropertiesAsserts {
     function clampLte(int256 a, int256 b) internal returns (int256) {
         if (!(a <= b)) {
             int256 value = b;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -417,8 +419,8 @@ abstract contract PropertiesAsserts {
                 "clampGt cannot clamp value a to be larger than uint256.max. Check your inputs/assumptions."
             );
             uint256 value = b + 1;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -436,8 +438,8 @@ abstract contract PropertiesAsserts {
     function clampGt(int256 a, int256 b) internal returns (int256) {
         if (!(a > b)) {
             int256 value = b + 1;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -455,8 +457,8 @@ abstract contract PropertiesAsserts {
     function clampGte(uint256 a, uint256 b) internal returns (uint256) {
         if (!(a > b)) {
             uint256 value = b;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -473,8 +475,8 @@ abstract contract PropertiesAsserts {
     function clampGte(int256 a, int256 b) internal returns (int256) {
         if (!(a > b)) {
             int256 value = b;
-            string memory aStr = PropertiesLibString.toString(a);
-            string memory valueStr = PropertiesLibString.toString(value);
+            string memory aStr = FuzzLibString.toString(a);
+            string memory valueStr = FuzzLibString.toString(value);
             bytes memory message = abi.encodePacked(
                 "Clamping value ",
                 aStr,
@@ -485,88 +487,5 @@ abstract contract PropertiesAsserts {
             return value;
         }
         return a;
-    }
-}
-
-/// @notice Efficient library for creating string representations of integers.
-/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/LibString.sol)
-/// @author Modified from Solady (https://github.com/Vectorized/solady/blob/main/src/utils/LibString.sol)
-/// @dev Name of the library is modified to prevent collisions with contract-under-test uses of LibString
-library PropertiesLibString {
-    function toString(int256 value) internal pure returns (string memory str) {
-        uint256 absValue = value >= 0 ? uint256(value) : uint256(-value);
-        str = toString(absValue);
-
-        if (value < 0) {
-            str = string(abi.encodePacked("-", str));
-        }
-    }
-
-    function toString(uint256 value) internal pure returns (string memory str) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            // The maximum value of a uint256 contains 78 digits (1 byte per digit), but we allocate 160 bytes
-            // to keep the free memory pointer word aligned. We'll need 1 word for the length, 1 word for the
-            // trailing zeros padding, and 3 other words for a max of 78 digits. In total: 5 * 32 = 160 bytes.
-            let newFreeMemoryPointer := add(mload(0x40), 160)
-
-            // Update the free memory pointer to avoid overriding our string.
-            mstore(0x40, newFreeMemoryPointer)
-
-            // Assign str to the end of the zone of newly allocated memory.
-            str := sub(newFreeMemoryPointer, 32)
-
-            // Clean the last word of memory it may not be overwritten.
-            mstore(str, 0)
-
-            // Cache the end of the memory to calculate the length later.
-            let end := str
-
-            // We write the string from rightmost digit to leftmost digit.
-            // The following is essentially a do-while loop that also handles the zero case.
-            // prettier-ignore
-            for { let temp := value } 1 {} {
-                // Move the pointer 1 byte to the left.
-                str := sub(str, 1)
-
-                // Write the character to the pointer.
-                // The ASCII index of the '0' character is 48.
-                mstore8(str, add(48, mod(temp, 10)))
-
-                // Keep dividing temp until zero.
-                temp := div(temp, 10)
-
-                 // prettier-ignore
-                if iszero(temp) { break }
-            }
-
-            // Compute and cache the final total length of the string.
-            let length := sub(end, str)
-
-            // Move the pointer 32 bytes leftwards to make room for the length.
-            str := sub(str, 32)
-
-            // Store the string's length at the start of memory allocated for our string.
-            mstore(str, length)
-        }
-    }
-
-    function toString(address value) internal pure returns (string memory str) {
-        bytes memory s = new bytes(40);
-        for (uint256 i = 0; i < 20; i++) {
-            bytes1 b = bytes1(
-                uint8(uint256(uint160(value)) / (2**(8 * (19 - i))))
-            );
-            bytes1 hi = bytes1(uint8(b) / 16);
-            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
-            s[2 * i] = char(hi);
-            s[2 * i + 1] = char(lo);
-        }
-        return string(s);
-    }
-
-    function char(bytes1 b) internal pure returns (bytes1 c) {
-        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
-        else return bytes1(uint8(b) + 0x57);
     }
 }
