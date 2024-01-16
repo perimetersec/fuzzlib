6387b2f9eacc2ab0c62ce53fc55aae9853e7acbd
ljz3 <kevin.ottawa@hotmail.com>
2024-01-16 00:08:37 -0500

    Create AssertWrapper.sol

---
+++ src/AssertWrapper.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.20;
+
+import './AssertHelper.sol';
+
+abstract contract EchidnaAsserts is AssertHelper {
+  function gt(uint256 a, uint256 b, string memory message) internal {
+    assertGt(a, b, message);
+  }
+
+  function lt(uint256 a, uint256 b, string memory message) internal {
+    assertLt(a, b, message);
+  }
+
+  function gte(uint256 a, uint256 b, string memory message) internal {
+    assertGte(a, b, message);
+  }
+
+  function lte(uint256 a, uint256 b, string memory message) internal {
+    assertLte(a, b, message);
+  }
+
+  function eq(uint256 a, uint256 b, string memory message) internal {
+    assertEq(a, b, message);
+  }
+
+  function neq(uint256 a, uint256 b, string memory message) internal {
+    assertNeq(a, b, message);
+  }
+
+  function t(bool a, string memory message) internal {
+    assertWithMsg(a, message);
+  }
+}
