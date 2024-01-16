f9115d6d2c5bf02b2bd5d90415687d4a2f9366aa
ljz3 <kevin.ottawa@hotmail.com>
2024-01-16 00:16:16 -0500

    Added license, changed pragma, formatting

--- src/AssertWrapper.sol
+++ src/AssertWrapper.sol
@@ -1,33 +1,34 @@
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import './AssertHelper.sol';
+import "./AssertHelper.sol";
 
 abstract contract EchidnaAsserts is AssertHelper {
-  function gt(uint256 a, uint256 b, string memory message) internal {
-    assertGt(a, b, message);
-  }
+    function gt(uint256 a, uint256 b, string memory message) internal {
+        assertGt(a, b, message);
+    }
 
-  function lt(uint256 a, uint256 b, string memory message) internal {
-    assertLt(a, b, message);
-  }
+    function lt(uint256 a, uint256 b, string memory message) internal {
+        assertLt(a, b, message);
+    }
 
-  function gte(uint256 a, uint256 b, string memory message) internal {
-    assertGte(a, b, message);
-  }
+    function gte(uint256 a, uint256 b, string memory message) internal {
+        assertGte(a, b, message);
+    }
 
-  function lte(uint256 a, uint256 b, string memory message) internal {
-    assertLte(a, b, message);
-  }
+    function lte(uint256 a, uint256 b, string memory message) internal {
+        assertLte(a, b, message);
+    }
 
-  function eq(uint256 a, uint256 b, string memory message) internal {
-    assertEq(a, b, message);
-  }
+    function eq(uint256 a, uint256 b, string memory message) internal {
+        assertEq(a, b, message);
+    }
 
-  function neq(uint256 a, uint256 b, string memory message) internal {
-    assertNeq(a, b, message);
-  }
+    function neq(uint256 a, uint256 b, string memory message) internal {
+        assertNeq(a, b, message);
+    }
 
-  function t(bool a, string memory message) internal {
-    assertWithMsg(a, message);
-  }
+    function t(bool a, string memory message) internal {
+        assertWithMsg(a, message);
+    }
 }
