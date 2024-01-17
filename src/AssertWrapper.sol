b370b9dfdc1a80df542c45f06d82ee48477aab41
ljz3 <kevin.ottawa@hotmail.com>
2024-01-17 12:00:22 -0500

    Added simplified assertion wrappers (#3)
    * Create AssertWrapper.sol
    
    * Added license, changed pragma, formatting
    
    * Added AssertWrapper to FuzzBase

---
+++ src/AssertWrapper.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AssertHelper.sol";
+
+abstract contract AssertWrapper is AssertHelper {
+    function gt(uint256 a, uint256 b, string memory message) internal {
+        assertGt(a, b, message);
+    }
+
+    function lt(uint256 a, uint256 b, string memory message) internal {
+        assertLt(a, b, message);
+    }
+
+    function gte(uint256 a, uint256 b, string memory message) internal {
+        assertGte(a, b, message);
+    }
+
+    function lte(uint256 a, uint256 b, string memory message) internal {
+        assertLte(a, b, message);
+    }
+
+    function eq(uint256 a, uint256 b, string memory message) internal {
+        assertEq(a, b, message);
+    }
+
+    function neq(uint256 a, uint256 b, string memory message) internal {
+        assertNeq(a, b, message);
+    }
+
+    function t(bool a, string memory message) internal {
+        assertWithMsg(a, message);
+    }
+}
--- src/FuzzBase.sol
+++ src/FuzzBase.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./Logging.sol";
 import "./Constants.sol";
-import "./AssertHelper.sol";
+import "./AssertWrapper.sol";
 import "./ClampWrapper.sol";
 
-abstract contract FuzzBase is AssertHelper, ClampWrapper, Logging, Constants {}
+abstract contract FuzzBase is AssertWrapper, ClampWrapper, Logging, Constants {}
