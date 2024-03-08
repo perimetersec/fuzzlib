2bb6c60b78193d137e02590fe4aa1c4770b8cd72
Rappie <jasper@rappie.com>
2024-03-08 20:24:45 +0100

    Add Math and Random helpers (#10)
    * Add more types to assertion wrapper functions
    
    * Add Math and Random helpers
    
    * Update src/MathHelper.sol
    
    Co-authored-by: 0xScourgedev <kevin.ottawa@hotmail.com>
    
    ---------
    
    Co-authored-by: 0xScourgedev <kevin.ottawa@hotmail.com>

---
+++ src/RandomHelper.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+abstract contract RandomHelper {
+    /// @notice Shuffle an array using Fisher-Yates algorithm
+    /// @dev Based on https://gist.github.com/scammi/602387a22e04c77beb73c0ebc0f0bc18
+    function shuffleArray(uint256[] memory shuffle, uint256 entropy) internal {
+        for (uint256 i = shuffle.length - 1; i > 0; i--) {
+            uint256 swapIndex = entropy % (shuffle.length - i);
+
+            uint256 currentIndex = shuffle[i];
+            uint256 indexToSwap = shuffle[swapIndex];
+
+            shuffle[i] = indexToSwap;
+            shuffle[swapIndex] = currentIndex;
+        }
+    }
+}
