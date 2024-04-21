fe03be49291b9b1b6c43267f81115f49df22e982
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-04-21 14:53:59 -0300

    Cosmetic changes

--- src/Onchain.sol
+++ src/Onchain.sol
@@ -11,8 +11,6 @@ enum Token {
 }
 
 contract Onchain {
-    error InvalidToken();
-
     IHevm internal constant vm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     address constant USDC_MAINNET = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -21,32 +19,28 @@ contract Onchain {
     address constant USDT_MAINNET = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address constant USDT_ARBITRUM = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
 
-    mapping(uint256 => mapping(Token => address)) chainIdToTokenToAddress;
+    mapping(Token => mapping(uint256 => address)) internal tokenToChainIdToAddress;
 
     constructor() {
-        chainIdToTokenToAddress[1][Token.USDC] = USDC_MAINNET;
-        chainIdToTokenToAddress[1][Token.USDT] = USDT_MAINNET;
-        chainIdToTokenToAddress[42161][Token.USDC] = USDC_ARBITRUM;
-        chainIdToTokenToAddress[42161][Token.USDT] = USDT_ARBITRUM;
+        tokenToChainIdToAddress[Token.USDC][1] = USDC_MAINNET;
+        tokenToChainIdToAddress[Token.USDT][1] = USDT_MAINNET;
+        tokenToChainIdToAddress[Token.USDC][42161] = USDC_ARBITRUM;
+        tokenToChainIdToAddress[Token.USDT][42161] = USDT_ARBITRUM;
     }
 
     function deal(Token token, address to, uint256 give) public {
+        address tokenAddress = tokenToChainIdToAddress[token][block.chainid];
         if (token == Token.USDC) {
-            address tokenAddress = chainIdToTokenToAddress[block.chainid][token];
             address masterMinter = IUSDC(tokenAddress).masterMinter();
             vm.prank(masterMinter);
             IUSDC(tokenAddress).configureMinter(address(this), give);
             IUSDC(tokenAddress).mint(to, give);
         } else if (token == Token.USDT) {
-            address tokenAddress = chainIdToTokenToAddress[block.chainid][token];
             address owner = IUSDT(tokenAddress).getOwner();
             vm.prank(owner);
             IUSDT(tokenAddress).issue(give);
             vm.prank(owner);
             IUSDT(tokenAddress).transfer(to, give);
-        }
-        else {
-            revert InvalidToken();
         }
     }
 }
