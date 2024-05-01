872bcdb768daa767c36a64478d808108a615cb04
Rappie <jasper@rappie.com>
2024-05-01 14:39:46 +0200

    Add library `LibLogging` for logging. Call library from `Logging` abstract contract.

--- src/Logging.sol
+++ src/Logging.sol
@@ -1,189 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {LibLogging} from "./libraries/LibLogging.sol";
+
 abstract contract Logging {
-    event Log(string debugString);
-    event LogString(string description, string data);
-    event LogString(string prefix, string description, string data);
-    event LogBytes(string description, bytes data);
-    event LogBytes(string prefix, string description, bytes data);
-    event LogUint(string description, uint256 data);
-    event LogUint(string prefix, string description, uint256 data);
-    event LogInt(string description, int256 data);
-    event LogInt(string prefix, string description, int256 data);
-    event LogAddress(string description, address data);
-    event LogAddress(string prefix, string description, address data);
-    event LogBool(string description, bool data);
-    event LogBool(string prefix, string description, bool data);
-    
-    event AssertionFailed();
-    event AssertionFailed(string description);
-    event AssertionFailed(string description, string data);
-    event AssertionFailed(string prefix, string description, string data);
-    event AssertionFailed(string description, bytes data);
-    event AssertionFailed(string prefix, string description, bytes data);
-    event AssertionFailed(string description, uint256 data);
-    event AssertionFailed(string prefix, string description, uint256 data);
-    event AssertionFailed(string description, int256 data);
-    event AssertionFailed(string prefix, string description, int256 data);
-    event AssertionFailed(string description, address data);
-    event AssertionFailed(string prefix, string description, address data);
-    event AssertionFailed(string description, bool data);
-    event AssertionFailed(string prefix, string description, bool data);
-
-    function log(string memory debugString) internal {
-        emit Log(debugString);
+    function log(string memory message) internal {
+        LibLogging.log(message);
     }
 
-    function log(string memory description, string memory data) internal {
-        emit LogString(description, data);
+    function log(string memory message, string memory data) internal {
+        LibLogging.log(message, data);
     }
 
-    function log(
-        string memory prefix,
-        string memory description,
-        string memory data
-    ) internal {
-        emit LogString(prefix, description, data);
+    function log(string memory message, bytes memory data) internal {
+        LibLogging.log(message, data);
     }
 
-    function log(string memory description, bytes memory data) internal {
-        emit LogBytes(description, data);
+    function log(string memory message, uint256 data) internal {
+        LibLogging.log(message, data);
     }
 
-    function log(
-        string memory prefix,
-        string memory description,
-        bytes memory data
-    ) internal {
-        emit LogBytes(prefix, description, data);
+    function log(string memory message, int256 data) internal {
+        LibLogging.log(message, data);
     }
 
-    function log(string memory description, uint256 data) internal {
-        emit LogUint(description, data);
+    function log(string memory message, address data) internal {
+        LibLogging.log(message, data);
     }
 
-    function log(
-        string memory prefix,
-        string memory description,
-        uint256 data
-    ) internal {
-        emit LogUint(prefix, description, data);
-    }
-
-    function log(string memory description, int256 data) internal {
-        emit LogInt(description, data);
-    }
-
-    function log(
-        string memory prefix,
-        string memory description,
-        int256 data
-    ) internal {
-        emit LogInt(prefix, description, data);
-    }
-
-    function log(string memory description, address data) internal {
-        emit LogAddress(description, data);
-    }
-
-    function log(
-        string memory prefix,
-        string memory description,
-        address data
-    ) internal {
-        emit LogAddress(prefix, description, data);
-    }
-
-    function log(string memory description, bool data) internal {
-        emit LogBool(description, data);
-    }
-
-    function log(
-        string memory prefix,
-        string memory description,
-        bool data
-    ) internal {
-        emit LogBool(prefix, description, data);
+    function log(string memory message, bool data) internal {
+        LibLogging.log(message, data);
     }
 
     function logFail() internal {
-        emit AssertionFailed();
+        LibLogging.logFail();
     }
 
-    function logFail(string memory debugString) internal {
-        emit AssertionFailed(debugString);
+    function logFail(string memory message) internal {
+        LibLogging.logFail(message);
     }
 
-    function logFail(string memory description, string memory data) internal {
-        emit AssertionFailed(description, data);
+    function logFail(string memory message, string memory data) internal {
+        LibLogging.logFail(message, data);
     }
 
-    function logFail(
-        string memory prefix,
-        string memory description,
-        string memory data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
+    function logFail(string memory message, bytes memory data) internal {
+        LibLogging.logFail(message, data);
     }
 
-    function logFail(string memory description, bytes memory data) internal {
-        emit AssertionFailed(description, data);
+    function logFail(string memory message, uint256 data) internal {
+        LibLogging.logFail(message, data);
     }
 
-    function logFail(
-        string memory prefix,
-        string memory description,
-        bytes memory data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
+    function logFail(string memory message, int256 data) internal {
+        LibLogging.logFail(message, data);
     }
 
-    function logFail(string memory description, uint256 data) internal {
-        emit AssertionFailed(description, data);
+    function logFail(string memory message, address data) internal {
+        LibLogging.logFail(message, data);
     }
 
-    function logFail(
-        string memory prefix,
-        string memory description,
-        uint256 data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
-    }
-
-    function logFail(string memory description, int256 data) internal {
-        emit AssertionFailed(description, data);
-    }
-
-    function logFail(
-        string memory prefix,
-        string memory description,
-        int256 data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
-    }
-
-    function logFail(string memory description, address data) internal {
-        emit AssertionFailed(description, data);
-    }
-
-    function logFail(
-        string memory prefix,
-        string memory description,
-        address data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
-    }
-
-    function logFail(string memory description, bool data) internal {
-        emit AssertionFailed(description, data);
-    }
-
-    function logFail(
-        string memory prefix,
-        string memory description,
-        bool data
-    ) internal {
-        emit AssertionFailed(prefix, description, data);
+    function logFail(string memory message, bool data) internal {
+        LibLogging.logFail(message, data);
     }
 }
---
+++ src/libraries/LibLogging.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library LibLogging {
+    event Log(string message);
+    event LogString(string message, string data);
+    event LogBytes(string message, bytes data);
+    event LogUint(string message, uint256 data);
+    event LogInt(string message, int256 data);
+    event LogAddress(string message, address data);
+    event LogBool(string message, bool data);
+    
+    event AssertionFailed();
+    event AssertionFailed(string message);
+    event AssertionFailed(string message, string data);
+    event AssertionFailed(string message, bytes data);
+    event AssertionFailed(string message, uint256 data);
+    event AssertionFailed(string message, int256 data);
+    event AssertionFailed(string message, address data);
+    event AssertionFailed(string message, bool data);
+
+    function log(string memory message) internal {
+        emit Log(message);
+    }
+
+    function log(string memory message, string memory data) internal {
+        emit LogString(message, data);
+    }
+
+    function log(string memory message, bytes memory data) internal {
+        emit LogBytes(message, data);
+    }
+
+    function log(string memory message, uint256 data) internal {
+        emit LogUint(message, data);
+    }
+
+    function log(string memory message, int256 data) internal {
+        emit LogInt(message, data);
+    }
+
+    function log(string memory message, address data) internal {
+        emit LogAddress(message, data);
+    }
+
+    function log(string memory message, bool data) internal {
+        emit LogBool(message, data);
+    }
+
+    function logFail() internal {
+        emit AssertionFailed();
+    }
+
+    function logFail(string memory message) internal {
+        emit AssertionFailed(message);
+    }
+
+    function logFail(string memory message, string memory data) internal {
+        emit AssertionFailed(message, data);
+    }
+
+    function logFail(string memory message, bytes memory data) internal {
+        emit AssertionFailed(message, data);
+    }
+
+    function logFail(string memory message, uint256 data) internal {
+        emit AssertionFailed(message, data);
+    }
+
+    function logFail(string memory message, int256 data) internal {
+        emit AssertionFailed(message, data);
+    }
+
+    function logFail(string memory message, address data) internal {
+        emit AssertionFailed(message, data);
+    }
+
+    function logFail(string memory message, bool data) internal {
+        emit AssertionFailed(message, data);
+    }
+}
