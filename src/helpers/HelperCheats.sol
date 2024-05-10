aaa66c4abd207c65178d4b96f6abca5d72b970ef
Rappie <jasper@rappie.com>
2024-05-10 17:24:10 +0200

    Add HelperCheats

---
+++ src/helpers/HelperCheats.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IHevm} from "../IHevm.sol";
+import {IStdCheats} from "../IStdCheats.sol";
+import {Constants} from "../Constants.sol";
+
+abstract contract HelperCheats {
+    IHevm internal vm = IHevm(Constants.ADDRESS_CHEATS); // echidna
+    IStdCheats internal mvm = IStdCheats(Constants.ADDRESS_CHEATS); // medusa
+
+    function warp(uint256 timestamp) public {
+        vm.warp(timestamp);
+    }
+
+    function roll(uint256 blockNumber) public {
+        vm.roll(blockNumber);
+    }
+
+    function prank(address sender) public {
+        vm.prank(sender);
+    }
+}
