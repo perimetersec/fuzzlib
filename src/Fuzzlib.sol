7fff22b10187bd4da64c3e0d507dda0f791f92f9
Rappie <jasper@rappie.com>
2024-05-10 17:18:11 +0200

    Add Fuzzlib

---
+++ src/Fuzzlib.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {HelperBase} from "./HelperBase.sol";
+import {HelperAssert} from "./HelperAssert.sol";
+import {HelperCheats} from "./HelperCheats.sol";
+import {HelperClamp} from "./HelperClamp.sol";
+
+contract Fuzzlib is
+    HelperBase,
+    HelperAssert,
+    HelperCheats,
+    HelperClamp
+{}
