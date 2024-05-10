236b017232627fbf4f4ce1d25d0fbf9863afd7b2
Rappie <jasper@rappie.com>
2024-05-10 17:27:18 +0200

    Update FuzzBase to `fl.` namespace style

--- src/FuzzBase.sol
+++ src/FuzzBase.sol
@@ -1,23 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AssertWrapper.sol";
-import "./ClampWrapper.sol";
-import "./MathHelper.sol";
-import "./RandomHelper.sol";
-import "./Logging.sol";
-import "./Constants.sol";
-import "./IHevm.sol";
-import "./IStdCheats.sol";
+import {Fuzzlib} from "./Fuzzlib.sol";
+import {PlatformCrytic} from "./platform/PlatformCrytic.sol";
 
-abstract contract FuzzBase is
-    AssertWrapper,
-    ClampWrapper,
-    MathHelper,
-    RandomHelper,
-    Logging,
-    Constants
-{
-    IHevm internal vm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D); // for echidna
-    IStdCheats internal mvm = IStdCheats(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D); // for medusa
+abstract contract FuzzBase {
+    Fuzzlib internal fl = new Fuzzlib();
+
+    constructor() {
+        fl.setPlatform(address(new PlatformCrytic()));
+    }
 }
