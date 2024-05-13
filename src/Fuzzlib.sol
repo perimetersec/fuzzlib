ca99962d190b713cf36c79b9e74d8de022c27447
Rappie <jasper@rappie.com>
2024-05-13 21:19:42 +0200

    Add  function to namespace

--- src/Fuzzlib.sol
+++ src/Fuzzlib.sol
@@ -5,10 +5,12 @@ import {HelperBase} from "./helpers/HelperBase.sol";
 import {HelperAssert} from "./helpers/HelperAssert.sol";
 import {HelperCheats} from "./helpers/HelperCheats.sol";
 import {HelperClamp} from "./helpers/HelperClamp.sol";
+import {HelperLog} from "./helpers/HelperLog.sol";
 
 contract Fuzzlib is
     HelperBase,
     HelperAssert,
     HelperCheats,
-    HelperClamp
+    HelperClamp,
+    HelperLog
 {}
