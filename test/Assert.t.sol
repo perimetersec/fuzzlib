5ac3f7823dbd6f3cabc33daeb487e71b670ef15d
Antonio Gabriel <AntonioGabriel@MacBook-Pro-de-David-2.local>
2024-05-23 14:08:59 -0300

    Changes on tests

--- test/Assert.t.sol
+++ test/Assert.t.sol
@@ -8,6 +8,7 @@ import {HelperAssert} from "../src/helpers/HelperAssert.sol";
 import {PlatformTest} from "./util/PlatformTest.sol";
 
 contract TestAsserts is Test, HelperAssert {
+
     function setUp() public {
         setPlatform(address(new PlatformTest()));
     }
@@ -26,33 +27,39 @@ contract TestAsserts is Test, HelperAssert {
         t(true, reason);
     }
 
-    function test_HelperAssert_Ttrue() public {
-        //vm.expectRevert();
+    function test_HelperAssert_t_true() public {
         t(true, "t reverts for true"); 
     }
 
-    function test_HelperAssert_Tfalse() public {
-        //emit AssertEqFail("Testing assertion");
+    function test_HelperAssert_t_false() public {
         vm.expectRevert();
         t(false, "t does not revert for true"); 
     }
 
-    function test_HelperAssert_eq_x_x_concrete(uint256 x) public {
-        assertEq(x, x,"eq reverts with the concrete values");
+    function test_HelperAssert_eq_x_x_concrete() public {
+        uint256 x = 1;
+        eq(x, x, "eq does not revert with equal values");
     }
 
-    function test_HelperAssert_eq_x_x_fuzz(uint256 x) public {
-        assertEq(x, x, "eq does not revert with the fuzz values");
+    function testFuzz_HelperAssert_eq_x_x_fuzz(uint256 x) public {
+        eq(x, x, "eq does not revert with the fuzz values");
     }
 
-   function test_HelperAssert_eq_x_y_concrete(uint256 x, uint256 y) public {
-        vm.assume(x == y);
-        assertEq(x, y,"eq reverts with the concrete values");
-    } 
+    function test_HelperAssert_eq_x_y_concrete() public {
+        uint256 x = 2;
+        uint256 y = 4;
+        vm.expectEmit(true, true, false, false);
+        emit AssertEqFail("");
+        vm.expectRevert();  
+        eq(x, y, "eq does not revert with the fuzz values");
+    }
 
-    function test_HelperAssert_eq_x_y_fuzz(uint256 x, uint256 y) public {
+    function testFuzz_HelperAssert_eq_x_y_fuzz(uint256 x, uint256 y) public {
         vm.assume(x != y);
+        string memory reason = "eq does not revert with equal values";
+        vm.expectEmit(true, true, false, false);
+        emit AssertEqFail("");
         vm.expectRevert();
-        eq(x,y, "eq does not revert with the fuzz values");
+        eq(x, y, reason);
     }
 }
