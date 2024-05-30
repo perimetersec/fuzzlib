af57c4b59df32ec2a34d8d17c612b3e682fbff73
Antonio Gabriel <AntonioGabriel@MacBook-Pro-de-David-2.local>
2024-05-30 14:14:36 -0300

    adjusts in the tests

--- test/Assert.t.sol
+++ test/Assert.t.sol
@@ -13,27 +13,16 @@ contract TestAsserts is Test, HelperAssert {
         setPlatform(address(new PlatformTest()));
     }
 
-    function test_TFail() public {
-        string memory reason = "Testing assertion";
-
-        vm.expectEmit(true, true, true, true);
-        emit AssertFail("Testing assertion");
-        vm.expectRevert(PlatformTest.TestAssertFail.selector);
-        t(false, reason);
-    }
-
-    function test_TSuccess() public {
-        string memory reason = "Testing assertion";
-        t(true, reason);
-    }
-
     function test_HelperAssert_t_true() public {
         t(true, "t does not revert for true"); 
     }
 
     function test_HelperAssert_t_false() public {
-        vm.expectRevert(PlatformTest.TestAssertFail.selector);
-        t(false, "t does not revert for true"); 
+        string memory reason = "t does not revert for true";
+        vm.expectEmit(true, true, false, false);
+        emit AssertFail(reason); 
+        vm.expectRevert(PlatformTest.TestAssertFail.selector);        
+        t(false, reason); 
     }
 
     function test_HelperAssert_eq_x_x_concrete() public {
@@ -48,17 +37,26 @@ contract TestAsserts is Test, HelperAssert {
     function test_HelperAssert_eq_x_y_concrete() public {
         uint256 x = 2;
         uint256 y = 4;
+        string memory reason = "eq reverts with different values";
+
         vm.expectEmit(true, true, false, false);
-        emit AssertEqFail("");
-        vm.expectRevert();  
-        eq(x, y, "eq reverts with different values");
+        emit AssertEqFail(reason);
+
+        vm.expectRevert(PlatformTest.TestAssertFail.selector);
+
+        eq(x, y, reason);
     }
 
     function testFuzz_HelperAssert_eq_x_y_fuzz(uint256 x, uint256 y) public {
         vm.assume(x != y);
+        string memory reason = "eq reverts with different values";
+
         vm.expectEmit(true, true, false, false);
-        emit AssertEqFail("");
-        vm.expectRevert();
-        eq(x, y,  "eq reverts with different values");
-    }
+        emit AssertEqFail(reason);
+
+        vm.expectRevert(PlatformTest.TestAssertFail.selector);
+
+        eq(x, y, reason);
+}
+
 }
