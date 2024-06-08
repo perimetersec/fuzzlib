d27c71d4c040213702e51f879c543655bab2b314
Rappie <jasper@rappie.com>
2024-06-08 18:20:08 +0200

    Remove `HelperCheats`

--- src/Fuzzlib.sol
+++ src/Fuzzlib.sol
@@ -3,14 +3,12 @@ pragma solidity ^0.8.0;
 
 import {HelperBase} from "./helpers/HelperBase.sol";
 import {HelperAssert} from "./helpers/HelperAssert.sol";
-import {HelperCheats} from "./helpers/HelperCheats.sol";
 import {HelperClamp} from "./helpers/HelperClamp.sol";
 import {HelperLog} from "./helpers/HelperLog.sol";
 
 contract Fuzzlib is
     HelperBase,
     HelperAssert,
-    HelperCheats,
     HelperClamp,
     HelperLog
 {}
