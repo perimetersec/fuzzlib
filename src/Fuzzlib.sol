d249e804f007d226b1e29c96cfdea350993e5093
Rappie <jasper@rappie.com>
2024-06-09 12:22:54 +0200

    Make math helpers available

--- src/Fuzzlib.sol
+++ src/Fuzzlib.sol
@@ -6,11 +6,13 @@ import {HelperAssert} from "./helpers/HelperAssert.sol";
 import {HelperCheats} from "./helpers/HelperCheats.sol";
 import {HelperClamp} from "./helpers/HelperClamp.sol";
 import {HelperLog} from "./helpers/HelperLog.sol";
+import {HelperMath} from "./helpers/HelperMath.sol";
 
 contract Fuzzlib is
     HelperBase,
     HelperAssert,
     HelperCheats,
     HelperClamp,
-    HelperLog
+    HelperLog,
+    HelperMath
 {}
