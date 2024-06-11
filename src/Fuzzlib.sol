490941b7246e58314ebb249157b590dd5a2d08c5
Rappie <jasper@rappie.com>
2024-06-11 20:59:25 +0200

    Remove `HelperCheats` (#42)
    * Make VMs available as global constant singleton
    
    * Remove `HelperCheats`
    
    * Rename singleton VM's back to `vm`

--- src/Fuzzlib.sol
+++ src/Fuzzlib.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import {HelperBase} from "./helpers/HelperBase.sol";
 import {HelperAssert} from "./helpers/HelperAssert.sol";
-import {HelperCheats} from "./helpers/HelperCheats.sol";
 import {HelperClamp} from "./helpers/HelperClamp.sol";
 import {HelperLog} from "./helpers/HelperLog.sol";
 import {HelperMath} from "./helpers/HelperMath.sol";
@@ -12,7 +11,6 @@ import {HelperRandom} from "./helpers/HelperRandom.sol";
 contract Fuzzlib is
     HelperBase,
     HelperAssert,
-    HelperCheats,
     HelperClamp,
     HelperLog,
     HelperMath,
