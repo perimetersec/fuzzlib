68585d90460820adee91d60e817aa0736a68a9a3
Antonio Gabriel <AntonioGabriel@MacBook-Pro-de-David-2.local>
2024-07-04 12:01:15 -0300

    adjusts of failReasons

--- test/Assert.t.sol
+++ test/Assert.t.sol
@@ -47,7 +47,7 @@ contract TestAsserts is Test, HelperAssert {
                 FuzzLibString.toString(y),
                 "!=",
                 reason
-            )));
+        )));
         vm.expectEmit(true, false, false, true);
         emit AssertEqFail(failReason);
 
@@ -66,7 +66,7 @@ contract TestAsserts is Test, HelperAssert {
                 FuzzLibString.toString(y),
                 "!=",
                 reason
-            )));
+        )));
         emit AssertEqFail(failReason);
 
         vm.expectRevert(PlatformTest.TestAssertFail.selector);
@@ -94,7 +94,7 @@ contract TestAsserts is Test, HelperAssert {
                 y ? "true" : "false",
                 "!=",
                 reason
-            )));
+        )));
         vm.expectEmit(true, false, false, true);
         emit AssertEqFail(failReason);
 
@@ -113,7 +113,7 @@ contract TestAsserts is Test, HelperAssert {
                 y ? "true" : "false",
                 "!=",
                 reason
-            )));
+        )));
         emit AssertEqFail(failReason);
 
         vm.expectRevert(PlatformTest.TestAssertFail.selector);
@@ -124,18 +124,18 @@ contract TestAsserts is Test, HelperAssert {
      /// @notice int256 version of eq test
     function test_eq_int_x_x() public {
         int256 x = 2;
-        eq(x, x, "eq does not revert with equal values");
+        eq(x, x, "example message");
     }
 
     function testFuzz_eq_int_x_x(int256 x) public {
-        eq(x, x, "eq does not revert with equal values");
+        eq(x, x, "example message");
     }
 
     function test_eq_int_x_y() public {
         int256 x = 2;
         int256 y = 4;
 
-        string memory reason = "eq reverts with different values.";
+        string memory reason = "example message";
         string memory failReason = string(abi.encodePacked(createAssertFailMessage(
                 FuzzLibString.toString(x),
                 FuzzLibString.toString(y),
@@ -149,12 +149,13 @@ contract TestAsserts is Test, HelperAssert {
 
         eq(x, y, reason);
     }
+
     function testFuzz_eq_int_x_y(int256 x, int256 y) public {
         vm.assume(x > type(int256).min / 2 && x < type(int256).max / 2);
         vm.assume(y > type(int256).min / 2 && y < type(int256).max / 2);
         vm.assume(x != y);
 
-        string memory reason = "eq reverts with fuzz values.";
+        string memory reason = "example message";
 
         vm.expectEmit(true, false, false, false);
         string memory failReason = string(abi.encodePacked(createAssertFailMessage(
