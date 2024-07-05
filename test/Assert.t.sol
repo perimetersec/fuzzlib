ae093bf72912b5322e780729c13b6bf27848319d
Antonio Gabriel <AntonioGabriel@MacBook-Pro-de-David-2.local>
2024-07-05 12:04:22 -0300

    only eq bool tests

--- test/Assert.t.sol
+++ test/Assert.t.sol
@@ -119,56 +119,5 @@ contract TestAsserts is Test, HelperAssert {
         vm.expectRevert(PlatformTest.TestAssertFail.selector);
 
         eq(x, y, reason);
-    }
-    
-     /// @notice int256 version of eq test
-    function test_eq_int_x_x() public {
-        int256 x = 2;
-        eq(x, x, "example message");
-    }
-
-    function testFuzz_eq_int_x_x(int256 x) public {
-        eq(x, x, "example message");
-    }
-
-    function test_eq_int_x_y() public {
-        int256 x = 2;
-        int256 y = 4;
-
-        string memory reason = "example message";
-        string memory failReason = string(abi.encodePacked(createAssertFailMessage(
-                FuzzLibString.toString(x),
-                FuzzLibString.toString(y),
-                "!=",
-                reason
-        )));
-        vm.expectEmit(true, false, false, true);
-        emit AssertEqFail(failReason);
-
-        vm.expectRevert(PlatformTest.TestAssertFail.selector);
-
-        eq(x, y, reason);
-    }
-
-    function testFuzz_eq_int_x_y(int256 x, int256 y) public {
-        vm.assume(x > type(int256).min / 2 && x < type(int256).max / 2);
-        vm.assume(y > type(int256).min / 2 && y < type(int256).max / 2);
-        vm.assume(x != y);
-
-        string memory reason = "example message";
-
-        vm.expectEmit(true, false, false, false);
-        string memory failReason = string(abi.encodePacked(createAssertFailMessage(
-                FuzzLibString.toString(x),
-                FuzzLibString.toString(y),
-                "!=",
-                reason
-        )));
-
-        emit AssertEqFail(failReason);
-
-        vm.expectRevert(PlatformTest.TestAssertFail.selector);
-
-        eq(x, y, reason);
     } 
 }
