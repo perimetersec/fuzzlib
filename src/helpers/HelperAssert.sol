f5e4db12c0e5a7e410fa5b33b38510c69489aba8
Rappie <jasper@rappie.com>
2025-07-01 12:52:48 +0200

    Add documentation

--- src/helpers/HelperAssert.sol
+++ src/helpers/HelperAssert.sol
@@ -5,7 +5,11 @@ import "./HelperBase.sol";
 
 import "../FuzzLibString.sol";
 
-/// @author Based on Crytic PropertiesHelper (https://github.com/crytic/properties/blob/main/contracts/util/PropertiesHelper.sol)
+/**
+ * @dev Assertion utilities for fuzzing operations.
+ * @author Perimeter <info@perimetersec.io>
+ * Based on Crytic PropertiesHelper (https://github.com/crytic/properties/blob/main/contracts/util/PropertiesHelper.sol)
+ */
 abstract contract HelperAssert is HelperBase {
     event AssertFail(string);
     event AssertEqFail(string);
@@ -15,6 +19,9 @@ abstract contract HelperAssert is HelperBase {
     event AssertLteFail(string);
     event AssertLtFail(string);
 
+    /**
+     * @dev Asserts that a boolean condition is true.
+     */
     function t(bool b, string memory reason) public {
         if (!b) {
             emit AssertFail(reason);
@@ -22,12 +29,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is equal to b. Violations are logged using reason.
-    function eq(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two unsigned integers are equal.
+     */
+    function eq(uint256 a, uint256 b, string memory reason) public {
         if (a != b) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -37,27 +42,23 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of eq
-    function eq(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two signed integers are equal.
+     */
+    function eq(int256 a, int256 b, string memory reason) public {
         if (a != b) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
             string memory assertMsg = createAssertFailMessage(aStr, bStr, "!=", reason);
-           emit AssertEqFail(assertMsg);
+            emit AssertEqFail(assertMsg);
             platform.assertFail();
         }
     }
 
-    /// @notice bool version of eq
-    function eq(
-        bool a,
-        bool b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two booleans are equal.
+     */
+    function eq(bool a, bool b, string memory reason) public {
         if (a != b) {
             string memory aStr = a ? "true" : "false";
             string memory bStr = b ? "true" : "false";
@@ -67,12 +68,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice address version of eq
-    function eq(
-        address a,
-        address b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two addresses are equal.
+     */
+    function eq(address a, address b, string memory reason) public {
         if (a != b) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -82,12 +81,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice bytes4 version of eq
-    function eq(
-        bytes4 a,
-        bytes4 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two bytes4 values are equal.
+     */
+    function eq(bytes4 a, bytes4 b, string memory reason) public {
         if (a != b) {
             bytes memory aBytes = abi.encodePacked(a);
             bytes memory bBytes = abi.encodePacked(b);
@@ -99,12 +96,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is not equal to b. Violations are logged using reason.
-    function neq(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two unsigned integers are not equal.
+     */
+    function neq(uint256 a, uint256 b, string memory reason) public {
         if (a == b) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -114,12 +109,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of neq
-    function neq(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that two signed integers are not equal.
+     */
+    function neq(int256 a, int256 b, string memory reason) public {
         if (a == b) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -129,12 +122,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is greater than or equal to b. Violations are logged using reason.
-    function gte(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first unsigned integer is greater than or equal to second.
+     */
+    function gte(uint256 a, uint256 b, string memory reason) public {
         if (!(a >= b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -144,12 +135,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of gte
-    function gte(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first signed integer is greater than or equal to second.
+     */
+    function gte(int256 a, int256 b, string memory reason) public {
         if (!(a >= b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -159,12 +148,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is greater than b. Violations are logged using reason.
-    function gt(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first unsigned integer is greater than second.
+     */
+    function gt(uint256 a, uint256 b, string memory reason) public {
         if (!(a > b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -174,12 +161,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of gt
-    function gt(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first signed integer is greater than second.
+     */
+    function gt(int256 a, int256 b, string memory reason) public {
         if (!(a > b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -189,12 +174,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is less than or equal to b. Violations are logged using reason.
-    function lte(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first unsigned integer is less than or equal to second.
+     */
+    function lte(uint256 a, uint256 b, string memory reason) public {
         if (!(a <= b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -204,12 +187,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of lte
-    function lte(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first signed integer is less than or equal to second.
+     */
+    function lte(int256 a, int256 b, string memory reason) public {
         if (!(a <= b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -219,12 +200,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice asserts that a is less than b. Violations are logged using reason.
-    function lt(
-        uint256 a,
-        uint256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first unsigned integer is less than second.
+     */
+    function lt(uint256 a, uint256 b, string memory reason) public {
         if (!(a < b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -234,12 +213,10 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    /// @notice int256 version of lt
-    function lt(
-        int256 a,
-        int256 b,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that first signed integer is less than second.
+     */
+    function lt(int256 a, int256 b, string memory reason) public {
         if (!(a < b)) {
             string memory aStr = FuzzLibString.toString(a);
             string memory bStr = FuzzLibString.toString(b);
@@ -249,50 +226,51 @@ abstract contract HelperAssert is HelperBase {
         }
     }
 
-    function assertRevertReasonNotEqual(
-        bytes memory returnData,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that revert reason is not equal to expected reason.
+     */
+    function assertRevertReasonNotEqual(bytes memory returnData, string memory reason) public {
         bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason);
         t(!isEqual, reason);
     }
 
-    function assertRevertReasonEqual(
-        bytes memory returnData,
-        string memory reason
-    ) public {
+    /**
+     * @dev Asserts that revert reason equals expected reason.
+     */
+    function assertRevertReasonEqual(bytes memory returnData, string memory reason) public {
         bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason);
         t(isEqual, reason);
     }
 
-    function assertRevertReasonEqual(
-        bytes memory returnData,
-        string memory reason1,
-        string memory reason2
-    ) public {
-        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason2);
-        string memory assertMsg = string(
-            abi.encodePacked(reason1, " OR ", reason2)
-        );
+    /**
+     * @dev Asserts that revert reason equals one of two expected reasons.
+     */
+    function assertRevertReasonEqual(bytes memory returnData, string memory reason1, string memory reason2) public {
+        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason2);
+        string memory assertMsg = string(abi.encodePacked(reason1, " OR ", reason2));
         t(isEqual, assertMsg);
     }
 
+    /**
+     * @dev Asserts that revert reason equals one of three expected reasons.
+     */
     function assertRevertReasonEqual(
         bytes memory returnData,
         string memory reason1,
         string memory reason2,
         string memory reason3
     ) public {
-        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason2) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason3);
-        string memory assertMsg = string(
-            abi.encodePacked(reason1, " OR ", reason2, " OR ", reason3)
-        );
+        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason2)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason3);
+        string memory assertMsg = string(abi.encodePacked(reason1, " OR ", reason2, " OR ", reason3));
         t(isEqual, assertMsg);
     }
 
+    /**
+     * @dev Asserts that revert reason equals one of four expected reasons.
+     */
     function assertRevertReasonEqual(
         bytes memory returnData,
         string memory reason1,
@@ -300,34 +278,33 @@ abstract contract HelperAssert is HelperBase {
         string memory reason3,
         string memory reason4
     ) public {
-        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason2) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason3) ||
-            FuzzLibString.isRevertReasonEqual(returnData, reason4);
-        string memory assertMsg = string(
-            abi.encodePacked(
-                reason1,
-                " OR ",
-                reason2,
-                " OR ",
-                reason3,
-                " OR ",
-                reason4
-            )
-        );
+        bool isEqual = FuzzLibString.isRevertReasonEqual(returnData, reason1)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason2)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason3)
+            || FuzzLibString.isRevertReasonEqual(returnData, reason4);
+        string memory assertMsg = string(abi.encodePacked(reason1, " OR ", reason2, " OR ", reason3, " OR ", reason4));
         t(isEqual, assertMsg);
     }
 
-    function createAssertFailMessage(string memory aStr, string memory bStr, string memory operator, string memory reason)internal pure returns (string memory) {
+    /**
+     * @dev Creates a formatted assertion failure message.
+     */
+    function createAssertFailMessage(
+        string memory aStr,
+        string memory bStr,
+        string memory operator,
+        string memory reason
+    ) internal pure returns (string memory) {
         return string(abi.encodePacked("Invalid: ", aStr, operator, bStr, ", reason: ", reason));
     }
 
-    // allow custom error only
-    function errAllow(
-        bytes4 errorSelector,
-        bytes4[] memory allowedErrors,
-        string memory message
-    ) public {
+    /**
+     * @dev Allows only specified custom errors by checking the error selector.
+     * @param errorSelector The 4-byte selector of the custom error that occurred
+     * @param allowedErrors Array of allowed custom error selectors
+     * @param message Context message for assertion failure
+     */
+    function errAllow(bytes4 errorSelector, bytes4[] memory allowedErrors, string memory message) public {
         bool allowed = false;
         for (uint256 i = 0; i < allowedErrors.length; i++) {
             if (errorSelector == allowedErrors[i]) {
@@ -337,13 +314,15 @@ abstract contract HelperAssert is HelperBase {
         }
         t(allowed, message);
     }
-    // require failure only
-    // check whether the errorData is an expected failure by checking the error message
-    function errAllow(
-        bytes memory errorData,
-        string[] memory allowedRequireErrorMessages,
-        string memory errorContext
-    ) public {
+    /**
+     * @dev Allows only specified require failure messages by extracting and comparing error strings.
+     * @param errorData Raw error data from failed function call
+     * @param allowedRequireErrorMessages Array of allowed require failure message strings
+     * @param errorContext Context message for assertion failure
+     */
+    function errAllow(bytes memory errorData, string[] memory allowedRequireErrorMessages, string memory errorContext)
+        public
+    {
         // space for error message without selector (4 bytes)
         bytes memory strippedData = new bytes(errorData.length - 4);
         assembly {
@@ -361,7 +340,7 @@ abstract contract HelperAssert is HelperBase {
 
             // now "strippedData" is the error message (string) without selector
         }
-        
+
         // extract the string from the remaining data
         string memory decodedString = abi.decode(strippedData, (string));
 
@@ -376,23 +355,22 @@ abstract contract HelperAssert is HelperBase {
 
         t(allowed, errorContext);
     }
-    
+
     /**
-     * Allow require failure & custom errors
-     * @param errorData: return data from a function call. In this case, it's a failure data
-     * @param allowedRequireErrorMessages: allowed require failure messages. It's a string array
-     * @param allowedCustomErrors: allowed custom errors. It can be just an array of 4 bytes function selector or it could be longer
-     * @param errorContext: error context: A message to describe the error
+     * @dev Allows specified require failures and custom errors by automatically detecting error type.
+     * @param errorData Raw error data from failed function call
+     * @param allowedRequireErrorMessages Array of allowed require failure message strings
+     * @param allowedCustomErrors Array of allowed custom error selectors
+     * @param errorContext Context message for assertion failure
      */
-    function errAllow(  
+    function errAllow(
         bytes memory errorData,
         string[] memory allowedRequireErrorMessages,
         bytes4[] memory allowedCustomErrors,
         string memory errorContext
     ) public {
-    
         bytes4 selector = bytes4(errorData);
-        
+
         if (_isErrorString(selector)) {
             // 1. require failure case (ex: require(false, "error message"))
             errAllow(errorData, allowedRequireErrorMessages, errorContext);
@@ -401,8 +379,10 @@ abstract contract HelperAssert is HelperBase {
             errAllow(selector, allowedCustomErrors, errorContext);
         }
     }
-    
-    // Check whether the selector is a require failure e.g.: require(false, "error message");
+
+    /**
+     * @dev Checks whether the selector is a require failure.
+     */
     function _isErrorString(bytes4 selector) internal pure returns (bool) {
         // 0x08c379a0 is the selector for "Error(string)" which is the error type for require(...) failure
         return selector == 0x08c379a0;
